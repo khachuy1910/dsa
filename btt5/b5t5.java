f4d260a9341fa8d78ee6f496d91c6449d707186e
Nguyễn Khắc Huy <khachuynguyen1910@gmail.com>
2025-11-12 13:34:59 +0700

    Create b5t5.java

---
+++ btt5/b5t5.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+    // Write your code here
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int n2 = Integer.parseInt(firstMultipleInput[1]);
+
+        int n3 = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] h1Temp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> h1 = new ArrayList<>();
+
+        for (int i = 0; i < n1; i++) {
+            int h1Item = Integer.parseInt(h1Temp[i]);
+            h1.add(h1Item);
+        }
+
+        String[] h2Temp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> h2 = new ArrayList<>();
+
+        for (int i = 0; i < n2; i++) {
+            int h2Item = Integer.parseInt(h2Temp[i]);
+            h2.add(h2Item);
+        }
+
+        String[] h3Temp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> h3 = new ArrayList<>();
+
+        for (int i = 0; i < n3; i++) {
+            int h3Item = Integer.parseInt(h3Temp[i]);
+            h3.add(h3Item);
+        }
+
+        int result = Result.equalStacks(h1, h2, h3);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
